e66a5064419b7eb89d86c3651a5d0e4e6c91b890
FangjieHung <j87345@gmail.com>
2026-07-11 10:39:20 +0800

    feat: 種子資料、repository tokens、date-utils、測試用 in-memory repo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { VEHICLE_REPO, CUSTOMER_REPO, BOOKING_REPO, MAINTENANCE_REPO } from './core/repositories/tokens';
+import { LocalStorageRepository } from './core/repositories/local-storage-repository';
+import { seedVehicles, seedCustomers, seedBookings, seedMaintenanceRecords } from './core/repositories/seed-data';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    { provide: VEHICLE_REPO, useFactory: () => new LocalStorageRepository('cr.vehicles', seedVehicles) },
+    { provide: CUSTOMER_REPO, useFactory: () => new LocalStorageRepository('cr.customers', seedCustomers) },
+    { provide: BOOKING_REPO, useFactory: () => new LocalStorageRepository('cr.bookings', seedBookings) },
+    { provide: MAINTENANCE_REPO, useFactory: () => new LocalStorageRepository('cr.maintenance', seedMaintenanceRecords) },
+  ],
 };
---
+++ src/app/core/repositories/tokens.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from '@angular/core';
+import { Vehicle, Customer, RentalBooking, MaintenanceRecord } from '../models';
+import { Repository } from './repository';
+
+export const VEHICLE_REPO = new InjectionToken<Repository<Vehicle>>('VEHICLE_REPO');
+export const CUSTOMER_REPO = new InjectionToken<Repository<Customer>>('CUSTOMER_REPO');
+export const BOOKING_REPO = new InjectionToken<Repository<RentalBooking>>('BOOKING_REPO');
+export const MAINTENANCE_REPO = new InjectionToken<Repository<MaintenanceRecord>>('MAINTENANCE_REPO');
